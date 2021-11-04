72f58b5af55f01f95540916631ccce599b9f1943
DonnieEC888 <donnie.ec888@gmail.com>
2021-11-04 19:14:46 -0400

    Created and styled offers, login, app and social links section

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -57,6 +57,7 @@ function pages() {
       layouts: 'src/layouts',
       partials: 'src/partials',
       helpers: 'src/helpers',
+      data: 'src/data'
     }))
     .pipe(inky())
     .pipe(gulp.dest('dist'));
